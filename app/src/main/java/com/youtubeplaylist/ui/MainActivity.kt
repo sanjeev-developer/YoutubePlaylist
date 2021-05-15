0bd15c43df98abecb721064a3848b84bf1b99380
sanjeev gupta <sanjeevgupta9223@gmail.com>
2021-05-14 21:36:36 -0700

    rebase 1.2

--- app/src/main/java/com/youtubeplaylist/ui/MainActivity.kt
+++ app/src/main/java/com/youtubeplaylist/ui/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DataTransfer{
 
         println("pushing master code to branchsecond 1")
         println("pushing master code to branchsecond 2")
+        println("pushing master code to branchsecond 3")
 
 
         vObj = ViewModelProvider(this@MainActivity).get(GoogleLogin::class.java)
